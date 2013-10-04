ab6c62c328478a69315a8f50ccd89a227cb5d120
Shannon Wynn <shannonwynn@mac.com>
2013-10-03 20:09:04 -0400

    Initial Commit

---
+++ Man's Best Friend/SWViewController.h
@@ -0,0 +1,13 @@
+//
+//  SWViewController.h
+//  Man's Best Friend
+//
+//  Created by Shannon Wynn on 10/3/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWViewController : UIViewController
+
+@end
